7aa288002c913abc74ad4e9b5f0b6f92fec23988
Stephen Pinkerton <stephenmpinkerton@gmail.com>
2015-03-30 18:08:15 -0500

    Add the bread and butter

---
+++ rgdb.py
@@ -0,0 +1,153 @@
+#!/usr/bin/python
+
+# Parts of this file liberally stolen from the below link (hence the copyright notice) 
+# https://github.com/paramiko/paramiko/blob/master/demos/interactive.py
+# I just hacked the CS 225 stuff into this script
+
+# Copyright (C) 2003-2007  Robey Pointer <robeypointer@gmail.com>
+#
+# This file is part of paramiko.
+#
+# Paramiko is free software; you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+#
+# Paramiko is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Paramiko; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+
+import os
+import shlex
+import socket
+import subprocess
+import sys
+import time
+
+import paramiko
+from paramiko.py3compat import u
+
+# windows does not have termios...
+try:
+    import termios
+    import tty
+    has_termios = True
+except ImportError:
+    has_termios = False
+
+
+def interactive_shell(chan):
+    if has_termios:
+        posix_shell(chan)
+    else:
+        windows_shell(chan)
+
+
+def posix_shell(chan):
+    import select
+    
+    oldtty = termios.tcgetattr(sys.stdin)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        tty.setcbreak(sys.stdin.fileno())
+        chan.settimeout(0.0)
+
+        while True:
+            r, w, e = select.select([chan, sys.stdin], [], [])
+            if chan in r:
+                try:
+                    x = u(chan.recv(1024))
+                    if len(x) == 0:
+                        sys.stdout.write('\r\n*** EOF\r\n')
+                        break
+                    sys.stdout.write(x)
+                    sys.stdout.flush()
+                except socket.timeout:
+                    pass
+            if sys.stdin in r:
+                x = sys.stdin.read(1)
+                if len(x) == 0:
+                    break
+                chan.send(x)
+
+    finally:
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, oldtty)
+
+    
+# thanks to Mike Looijmans for this code
+def windows_shell(chan):
+    import threading
+
+    sys.stdout.write("Line-buffered terminal emulation. Press F6 or ^Z to send EOF.\r\n\r\n")
+        
+    def writeall(sock):
+        while True:
+            data = sock.recv(256)
+            if not data:
+                sys.stdout.write('\r\n*** EOF ***\r\n\r\n')
+                sys.stdout.flush()
+                break
+            sys.stdout.write(data)
+            sys.stdout.flush()
+        
+    writer = threading.Thread(target=writeall, args=(chan,))
+    writer.start()
+        
+    try:
+        while True:
+            d = sys.stdin.read(1)
+            if not d:
+                break
+            chan.send(d)
+    except EOFError:
+        # user hit ^Z or F6
+        pass
+
+
+def commit_locally():
+    cmd = shlex.split("svn ci -m \"%d\"" % (int(time.time())))
+    print subprocess.check_output(cmd)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        raise Exception("run with: python rgdb.py [mp_name]")
+    print "WARNING! This script commits your local changes to EWS."
+    client = paramiko.SSHClient()
+    client._policy = paramiko.WarningPolicy()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    ssh_config = paramiko.SSHConfig()
+    user_config_file = os.path.expanduser("~/.ssh/config")
+    if os.path.exists(user_config_file):
+        with open(user_config_file) as f:
+            ssh_config.parse(f)
+
+    options = ssh_config.lookup("ews")
+    cfg = {'hostname': options['hostname'], 'username': options["user"]}
+
+    user_config = ssh_config.lookup(cfg['hostname'])
+    for k in ('hostname', 'username', 'port'):
+        if k in user_config:
+            cfg[k] = user_config[k]
+
+    if 'proxycommand' in user_config:
+        cfg['sock'] = paramiko.ProxyCommand(user_config['proxycommand'])
+
+    try:
+        commit_locally()
+        client.connect(**cfg)
+        remote_shell = client.invoke_shell()
+        mp_name = sys.argv[1]
+        cmds = ["cd ~/Documents/cs225/%s" % (mp_name), "svn up", "make", "gdb %s" % (mp_name)]
+        for cmd in cmds:
+            remote_shell.send(cmd + "\n")
+                
+        interactive_shell(remote_shell)
+    except Exception as e:
+        print e
